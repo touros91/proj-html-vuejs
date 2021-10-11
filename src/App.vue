c375785ccc5fd389acd2e7cb8429381f242fbd05
touros91 <andreageraci91@gmail.com>
2021-10-11 12:36:46 +0200

    Aggiunto Header dinamico

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Header/>
+    <Header :srcLogo="require('./assets/img/logo.png')" :altLogo="'Fable Logo'" :nav="menu"/>
     <Main/>
     <Footer/>
   </div>
@@ -17,10 +17,59 @@ export default {
     Header,
     Footer,
     Main
+  },
+  data (){
+        return {
+            menu: [
+                {
+                  title: 'Home',
+                  img: 'home.png',
+                  imgWhite: 'home-2.png',
+                  alt: 'Home' 
+                },
+                {
+                  title: 'Pages',
+                  img: 'document.png',
+                  imgWhite: 'document-2.png',
+                  alt: 'Pages'
+                },
+                {
+                  title: 'Blog',
+                  img: 'printer.png',
+                  imgWhite: 'printer-2.png',
+                  alt: 'Blog'
+                },
+                {
+                    title: 'Shop',
+                    img: 'cart.png',
+                    imgWhite: 'cart-2.png',
+                    alt: 'Shop'
+                },
+                {
+                  title: 'Shortcodes',
+                  img: 'lab.png',
+                  imgWhite: 'lab-2.png',
+                  alt: 'Shortcodes'
+                },
+                {
+                  title: 'Support',
+                  img: 'chat.png',
+                  imgWhite: 'chat-2.png',
+                  alt: 'Support'
+                },
+                {
+                  title: 'Contact',
+                  img: 'envelope.png',
+                  imgWhite: 'envelope-2.png',
+                  alt: 'Contact'
+                }
+            ]
+        }
   }
 }
 </script>
 
 <style lang="scss">
+@import './assets/style/common';
 
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,83 @@
 <template>
     <header>
-        
+        <div class="container-80">
+            <div class="logo">
+                <img :src="srcLogo" :alt="altLogo">
+            </div>
+            <div class="nav-menu">
+                <ul>
+                    <li v-for="(icon, index) in nav" :key="index">
+                        <div class="icon-image">
+                            <img :src="require('../assets/img/' + icon.img)" :alt="icon.alt">
+                        </div>
+                        <div class="icon-image-white">
+                            <img :src="require('../assets/img/' + icon.imgWhite)" :alt="icon.alt">
+                        </div> 
+                        <div>{{icon.title}}</div>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </header>
-  
 </template>
 
 <script>
 export default {
-    name: 'Header'
+    name: 'Header',
+    props: ['srcLogo', 'altLogo', 'nav']
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '../assets/style/common';
 
+header {
+    font-size: 15px;
+    .container-80 {
+        display: flex;
+        justify-content: space-between;
+        .logo {
+            max-width: 180px;
+            img {
+                width: 100%;
+            }
+        }
+        .nav-menu {
+            width: 70%;
+            ul {
+                display: flex;
+                li {
+                    list-style: none;
+                    display: inline-block;
+                    padding: 30px;
+                    width: 100px;
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    color: $mainColor;
+                    .icon-image, .icon-image-white {
+                        max-width: 30px;
+                        margin-bottom: 10px;
+                        img {
+                            width: 100%;
+                        }
+                    }
+                    .icon-image-white {
+                        display: none;
+                    }
+                }
+                li:hover {
+                    background-color: #FE6500;
+                    color: white;
+                }
+                li:hover .icon-image {
+                    display: none;
+                }
+                li:hover .icon-image-white {
+                    display: block;
+                }
+            }
+        }   
+    }
+}
 </style>
