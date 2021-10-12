7f91a7fabc29ced882960374443e5ae965f5a374
touros91 <andreageraci91@gmail.com>
2021-10-12 15:56:21 +0200

    Aggiunto componente dinamico ContactDetails footer

---
+++ src/components/Footer/ContactDetails.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="container-80">
+        <div class="icon-box">
+            <div class="icon" v-for="(contact, index) in contactInfo" :key="index">
+                <div class="image">
+                    <img :src="require('../../assets/img/' + contact.icon)" :alt="contact.name">
+                </div>
+                <h3>{{contact.name}}</h3>
+                <p>{{contact.details}}</p>
+            </div>
+        </div>
+    </div> 
+</template>
+
+<script>
+export default {
+    name: 'ContactDetails',
+    props: ['contactInfo']
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/style/common';
+
+.container-80 {
+    .icon-box {
+        display: flex;
+        .icon {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            padding: 10px;
+            h3 {
+                text-align: center;
+                color: $mainColor;
+                margin: 20px;
+                font-weight: 400;
+                font-size: 22px;
+            }
+            p {
+                color: $grayText;
+                font-weight: 500;
+                text-align: center;
+            }
+            .image {
+                border-radius: 50%;
+                margin: auto;
+                width: 90px;
+                height: 90px;
+                position: relative;
+                background-color: $white;
+                color: $white;
+                img {
+                    padding: 5px;
+                    width: 50%;
+                    position: absolute;
+                    top: 50%;
+                    left: 50%;
+                    transform: translate(-50%, -50%);        
+                }
+            }
+        
+        }
+    }
+}
+</style>
--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,20 +1,77 @@
 <template>
     <footer>
         <GoogleMaps/>
+        <section>
+            <div class="container-80">
+                <div class="title">
+                    <h1>Contact Us</h1>
+                    <h3>Our postal address and contact details</h3>
+                    <img src="../../assets/img/header_divider.png" alt="Divider">
+                </div>
+            </div>
+        </section>
+        <ContactDetails :contactInfo="contacts"/>
     </footer>
 </template>
 
 <script>
 import GoogleMaps from './GoogleMaps.vue';
+import ContactDetails from './ContactDetails.vue';
 
 export default {
     name: 'Footer',
     components: {
-        GoogleMaps
+        GoogleMaps,
+        ContactDetails
+    },
+    data() {
+        return {
+            contacts: [
+                {
+                    icon: 'document_orange.png',
+                    name: 'Postal Address',
+                    details: 'Fable Care Center 85 Fentiman Ave Ottawa, ON K1S 0T7'
+                },
+                {
+                    icon: 'envelope_orange.png',
+                    name: 'Phone & E-mail',
+                    details: 'Phone: 1-800-64-38 Fax: 1-800-64-39 office@fable.com'
+                },
+                {
+                    icon: 'mobile.png',
+                    name: 'Business Hours',
+                    details: 'Monday - Friday 8.00 am - 5.00 pm Weekend Closed'
+                },
+                {
+                    icon: 'clock.png',
+                    name: 'Sessions',
+                    details: 'Mornings, 8 am - 12 noon Afternoons, 1 pm - 5 pm Full Day, 8 am - 5 pm'
+                }
+            ]
+        }
     }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '../../assets/style/common';
 
+.container-80 {
+    padding-top: 30px;
+    .title {
+        text-align: center;
+        h1 {
+            color: $mainColor;
+            font-size: 45px;
+            font-weight: 300;
+        }
+        h3 {
+            font-weight: 500;
+            color: #BBBBBB;
+            font-size: 17px;
+            margin-top: 10px;
+            margin-bottom: 35px;
+        }
+    }  
+}
 </style>
